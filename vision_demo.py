12af29c8c5a1af31eabe6e986ec3c65b8255648c
chinmayi7 <60067411+chinmayi7@users.noreply.github.com>
2020-03-23 10:32:39 +0530

    Add files via upload
    detect text in image

--- vision_demo.py
+++ vision_demo.py
@@ -1,16 +1,29 @@
 import io
 import os
+import pandas as pd
 from google.cloud import vision
 from google.cloud.vision import types
-
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'key.json'
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'C:\\Users\\User\\Documents\\gcloudstuff\\apikeys.json'
 client = vision.ImageAnnotatorClient()
-FILE_NAME = 'text1.jpg'
-FOLDER_PATH = r'C:\Users\User\Documents\icr_vision_demo\venv\texts'
- 
-with io.open(os.path.join(FOLDER_PATH,FILE_NAME), 'rb') as image_file:
-    content = iamge_file.read()
-
+def detectText(img):
+    with io.open(img,'rb') as image_file:
+        content = image_file.read()
     image = vision.types.Image(content=content)
-
     response = client.text_detection(image=image)
+    texts = response.text_annotations
+    df = pd.DataFrame(columns=['locale','description'])
+    for text in texts:
+        df = df.append(
+            dict(
+                locale=text.locale,
+                description=text.description 
+            ),
+            ignore_index = True
+        )
+    return df
+
+    print (df['description'][0])
+FILE_NAME = 'text3.jpg'
+FOLDER_PATH = r'C:\Users\User\Documents\gcloudstuff\visionex'
+print (detectText(os.path.join(FOLDER_PATH,FILE_NAME)))
+    
